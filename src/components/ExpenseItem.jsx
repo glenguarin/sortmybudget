7ee6d25fe83669787ef597ad09ecf4d22f5c76b1
Glen Guarin <glen.guarin@gmail.com>
2023-10-04 16:08:16 +1300

    added expenseitem

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,5 +1,44 @@
-const ExpenseItem = () => {
-  return <div>Expenses</div>;
+// rrd imports
+import { Link, useFetcher } from "react-router-dom";
+
+// helper imports
+import {
+  formatCurrency,
+  formatDateToLocaleString,
+  getAllMatchingItems,
+} from "../helpers";
+
+const ExpenseItem = ({ expense, showBudget }) => {
+  const fetcher = useFetcher();
+
+  const budget = getAllMatchingItems({
+    category: "budgets",
+    key: "id",
+    value: expense.budgetId,
+  })[0];
+
+  return (
+    <>
+      <td>{expense.name}</td>
+      <td>{formatCurrency(expense.amount)}</td>
+      <td>{formatDateToLocaleString(expense.createdAt)}</td>
+      {showBudget && (
+        <td>
+          <Link to={`/budget/${budget.id}`}>{budget.name}</Link>
+        </td>
+      )}
+      <td>
+        <fetcher.Form method="post">
+          <input type="hidden" name="_action" value="deleteExpense" />
+          <input type="hidden" name="expenseId" value={expense.id} />
+          <button
+            type="submit"
+            aria-label={`Delete ${expense.name} expense`}
+          ></button>
+        </fetcher.Form>
+      </td>
+    </>
+  );
 };
 
 export default ExpenseItem;
